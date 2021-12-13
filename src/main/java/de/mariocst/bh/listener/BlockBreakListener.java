e14b4167239ac39510d17dbb7c2cdd03d092359a
marioCST <marioCST@noreply.github.com>
2021-12-13 22:07:37 +0100

    If you break a spawner, you now get the spawn egg of the spawntype mob

--- src/main/java/de/mariocst/bh/listener/BlockBreakListener.java
+++ src/main/java/de/mariocst/bh/listener/BlockBreakListener.java
@@ -1,11 +1,16 @@
 package de.mariocst.bh.listener;
 
+import de.mariocst.bh.BuildHub;
 import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class BlockBreakListener implements Listener {
     @EventHandler
@@ -14,7 +19,16 @@ public class BlockBreakListener implements Listener {
 
         if (event.getBlock().getType() == Material.SPAWNER) {
             if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE || player.getInventory().getItemInMainHand().getType() == Material.NETHERITE_PICKAXE) {
+                CreatureSpawner spawner = (CreatureSpawner) event.getBlock().getState();
+                ItemStack stack = new ItemStack(Objects.requireNonNull(Material.getMaterial(spawner.getSpawnedType() + "_SPAWN_EGG")), 1);
+
                 event.setExpToDrop(0);
+                if (spawner.getSpawnedType() == EntityType.PIG) {
+                    player.sendMessage(BuildHub.getInstance().getPrefix() + "Damit man nicht dupen kann, kann man keine Pig Spawn Eggs bekommen!");
+                }
+                else {
+                    player.getInventory().addItem(stack);
+                }
                 player.getInventory().addItem(new ItemStack(Material.SPAWNER, 1));
             }
         }
