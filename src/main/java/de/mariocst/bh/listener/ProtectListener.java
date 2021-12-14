7a3339ad0b40c61b4cebb1a03b6746aef97bf829
marioCST <marioCST@noreply.github.com>
2021-12-14 21:59:08 +0100

    Item Frames are now protected

--- src/main/java/de/mariocst/bh/listener/ProtectListener.java
+++ src/main/java/de/mariocst/bh/listener/ProtectListener.java
@@ -1,6 +1,7 @@
 package de.mariocst.bh.listener;
 
 import de.mariocst.bh.BuildHub;
+import io.papermc.paper.event.player.PlayerItemFrameChangeEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,6 +32,19 @@ public class ProtectListener implements Listener {
                 && event.getBlock().getZ() <= 423 && event.getBlock().getZ() >= 391) {
             event.setCancelled(true);
             player.sendMessage(BuildHub.getInstance().getPrefix() + "§cDu darfst hier nicht bauen!");
+        }
+    }
+
+    @EventHandler
+    public void onItemFrameChange(PlayerItemFrameChangeEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.hasPermission("mario.protect.itemframe") || player.hasPermission("mario.*") || player.hasPermission("*") || player.isOp()) return;
+
+        if (event.getItemFrame().getLocation().getX() <= -753 && event.getItemFrame().getLocation().getX() >= -783
+                && event.getItemFrame().getLocation().getZ() <= 423 && event.getItemFrame().getLocation().getZ() >= 391) {
+            event.setCancelled(true);
+            player.sendMessage(BuildHub.getInstance().getPrefix() + "§cDu darfst hier nicht mit Item Frames interagieren!");
         }
     }
 }
